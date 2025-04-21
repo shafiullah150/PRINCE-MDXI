666ed7998251e95e00fba089768c5ccff5446a28
shafiullah150 <unknownkhan150@gmail.com>
2025-04-21 17:41:48 +0500

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
-AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY PRINCE-MDX *",
 // set the auto reply massage on status reply  
@@ -23,7 +23,7 @@ STICKER_NAME: process.env.STICKER_NAME || "PRINCE MDX",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "🙂,🤌🏻,😩,🫀,🫂,🌏,🎀,😒,🌈,🫠,🕊️,⛱️,✨,🗿,💫,🩵,🌷,🌸,♥️,🪻,🤐,🫶🏻,🫣,🤒,🤠,😽,😸,😹,👊🏻,🫀,💍,👑,🙊,🐱,🐼,🦋,🪼,🐋,🪽,🦚,🕊️,🌴,☘️,🌿,🍁,🪺,🍃,🍂,🐚,🪸,🥀,🌾,🪷,🌝,🌸,🌚,🌼,🌻,✨,💥,🔥,☃️,💦,🌬️,🫧,🫐,🍡,🍿,🥂,🥢,🪀,🥊,🏵️,🏆,🎗️,🎧,🪇,🎭,🎯,🎻,🎷,🚔,🛩️,🚀,🗿,🎠,⛰️,⛱️,⛺,🔦,🕯️,🪔,💰,💎,💣,🔫,🚬,🔮,📿,🧸,🎈,🎁,🎊,🎉,🏮,🪅,🔖,📍,🖇️,🔗,🔐,💔,〽️,💯,🚩,🏴‍☠️,😊,☺️,🥲,😆,😃,🙃,🙂,😍,😗,🤩,😎,😢,😭,😳,😓,😬,🙄,🤧,🥴,👻,🤡,☠️,💀,😘,😜,🤓,🙂‍↕,🥺,🥱,🩷,❤️,🧡,💛,💚,🩵,💙,💜,🖤,🩶,🤍,🤎,💔,❤‍🔥,❤‍🩹,❣️,💕,💓,💗,💖,💝,♥️,",
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
@@ -41,7 +41,7 @@ WELCOME_ENABLED: process.env.WELCOME_ENABLED || "true",
     // welcome group members 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
@@ -59,7 +59,7 @@ ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
 PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
 // make false if want private mod
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
+AUTO_TYPING: process.env.AUTO_TYPING || "true",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
@@ -69,6 +69,6 @@ ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
 ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same", 
 // change it to 'same' if you want to resend deleted message in same chat 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+AUTO_RECORDING: process.env.AUTO_RECORDING || "true"
 // make it true for auto recoding 
 };
